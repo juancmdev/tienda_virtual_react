667c3b2fcbbe7575c3da65a82b358797dd4e1df3
juancmdev <juancmorenoj@gmail.com>
2025-10-11 21:25:58 -0500

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-
-const ProductCard = () => {
+const ProductCard = ({ id, nombre, descripcion, urlImagen, precio }) => {
+  // Ahora podemos usar nombre, precio, etc., directamente
   return <div>ProductCard</div>;
 };
 
