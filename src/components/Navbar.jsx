f2222d2f8a6a4e67b44769a660a3016b39f562f8
juancmdev <juancmorenoj@gmail.com>
2025-10-11 21:22:46 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,32 @@ const Navbar = () => {
           <div className="text-xl font-bold">Mi Tienda</div>
 
           {/* Lado Derecho: Los Enlaces de Navegación */}
-          <nav>{/* Aquí irán los enlaces: Home, Productos, Carrito */}</nav>
+          <nav>
+            {/* Aquí irán los enlaces: Home, Productos, Carrito */}
+            <ul className="flex gap-4">
+              <li>
+                <a
+                  href="/productos"
+                  className="hover:text-orange-400 transition"
+                >
+                  Productos
+                </a>
+              </li>
+              <li>
+                <a href="/carrito" className="hover:text-orange-400 transition">
+                  Carrito
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/contacto"
+                  className="hover:text-orange-400 transition"
+                >
+                  Contacto
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
     </>
