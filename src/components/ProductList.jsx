4e69fada8bc99e87a17fbb32b2b1f94a6fd8ef56
juancmdev <juancmorenoj@gmail.com>
2025-10-12 11:11:27 -0500

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,53 +1,63 @@
+import { useState } from "react";
 import ProductCard from "./ProductCard";
 
+const productos = [
+  {
+    id: 1,
+    nombre: "Producto 1",
+    descripcion: "Descripción del Producto 1",
+    urlImagen:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
+    precio: 19.99,
+  },
+  {
+    id: 2,
+    nombre: "Producto 2",
+    descripcion: "Descripción del Producto 2",
+    urlImagen:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
+    precio: 29.99,
+  },
+  {
+    id: 3,
+    nombre: "Producto 3",
+    descripcion: "Descripción del Producto 3",
+    urlImagen:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
+    precio: 5.99,
+  },
+
+  {
+    id: 4,
+    nombre: "Producto 4",
+    descripcion: "Descripción del Producto 4",
+    urlImagen:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
+    precio: 5.99,
+  },
+];
+
 const ProductList = () => {
-  const productos = [
-    {
-      id: 1,
-      nombre: "Producto 1",
-      descripcion: "Descripción del Producto 1",
-      urlImagen:
-        "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
-      precio: 19.99,
-    },
-    {
-      id: 2,
-      nombre: "Producto 2",
-      descripcion: "Descripción del Producto 2",
-      urlImagen:
-        "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
-      precio: 29.99,
-    },
-    {
-      id: 3,
-      nombre: "Producto 3",
-      descripcion: "Descripción del Producto 3",
-      urlImagen:
-        "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
-      precio: 5.99,
-    },
-
-    {
-      id: 4,
-      nombre: "Producto 4",
-      descripcion: "Descripción del Producto 4",
-      urlImagen:
-        "https://http2.mlstatic.com/D_NQ_NP_2X_752793-MCO89741436545_082025-F.webp",
-      precio: 5.99,
-    },
-  ];
-
   const [searchTerm, setSearchTerm] = useState("");
 
+  //Lógica de filtrado
   const productosFiltrados = productos.filter((producto) =>
     producto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
-    <div className="container mx-auto p-4">
-      {/* Aplicamos Flexbox/Grid para el listado */}
+    <div className="container mx-auto p-8">
+      {/*Barra de búsqueda */}
+      <input
+        type="text"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Buscar Productos..."
+        className="w-full p-3 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+      />
+
+      {/* Grid de productos filtrados */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {productos.map((producto) => (
+        {productosFiltrados.map((producto) => (
           // Usamos la propiedad `key` con el id único del producto
           <ProductCard
             key={producto.id}
