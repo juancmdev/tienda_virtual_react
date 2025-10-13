5740289cac0f4ba890da1a7bbb0768244e37a2ce
juancmdev <juancmorenoj@gmail.com>
2025-10-13 12:35:29 -0500

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import AdminPage from "./pages/AdminPage";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <ProductList />
       <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="home" element={<Home />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/productos" element={<ProductList />} />
+        <Route path="contacto" element={<Contact />} />
       </Routes>
     </>
   );
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"; // <--- ¡Importamos Outlet!
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-gray-800 text-white p-4">
         {/* Por ahora, solo texto temporal */}
-        <p>Header (Barra de Navegación)</p>
+        <Navbar />
       </header>
 
       <main className="flex-grow container mx-auto p-4">
