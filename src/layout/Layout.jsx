c8ceef719f4d40e79b2b12931e3ff7668095ca3d
juancmdev <juancmorenoj@gmail.com>
2025-10-13 12:24:12 -0500

    Layout updated

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom"; // <--- ¡Importamos Outlet!
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-gray-800 text-white p-4">
+        {/* Por ahora, solo texto temporal */}
+        <p>Header (Barra de Navegación)</p>
+      </header>
+
+      <main className="flex-grow container mx-auto p-4">
+        {/* ¡Aquí se renderizará el contenido de la ruta anidada! */}
+        <Outlet />
+      </main>
+
+      <footer className="bg-gray-800 text-white p-4 text-center">
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
