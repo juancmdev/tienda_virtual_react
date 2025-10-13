d1b7e26b8e111dcbf94f6123d5358b3c76d73b3e
juancmdev <juancmorenoj@gmail.com>
2025-10-13 16:23:25 -0500

    Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/home" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/productos" element={<ProductList />} />
           <Route path="contacto" element={<Contact />} />
