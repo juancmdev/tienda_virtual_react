6d8833ab7759064264aef18b26dc0c244d7cc89c
juancmdev <juancmorenoj@gmail.com>
2025-10-14 19:10:20 -0500

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -103,7 +103,7 @@ const LoginPage = () => {
         {/* Botón de Enviar */}
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white p-3 rounded-lg font-bold hover:bg-blue-700 transition duration-200"
+          className="w-full bg-blue-600 text-white p-3 rounded-lg font-bold hover:bg-blue-700 transition duration-200 cursor-pointer"
         >
           Iniciar Sesión
         </button>
