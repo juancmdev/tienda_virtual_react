84a95996edc255e4fd1e6278cdd33d4c2fc33c47
juancmdev <juancmorenoj@gmail.com>
2025-10-17 22:45:38 -0500

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,23 +1,32 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+    },
+    urlImagen: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+    },
+    categoria: {
+      // <-- ¡NUEVO CAMPO AÑADIDO!
+      type: String,
+      required: false, // Por ahora, lo dejamos opcional
+      trim: true,
+    },
   },
-  descripcion: {
-    type: String,
-    required: true,
-  },
-  urlImagen: {
-    type: String,
-    required: true,
-  },
-  precio: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Producto", productSchema);
 
